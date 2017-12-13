6a27faaf2d03f7ed4b42c82354b2743ab2d0aeb6
OdooMod <542525874@qq.com>
2017-12-13 13:13:21 +0800

    account_partner_patch optimize

--- account_partner_patch/models/res_partner.py
+++ account_partner_patch/models/res_partner.py
@@ -2,7 +2,7 @@
 # Copyright 2017 Jarvis (www.odoomod.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import models, _, fields
 
 
 class res_partner(models.Model):
